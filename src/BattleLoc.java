fd5aaea140d0f9324a2bc7489d01e9c8eb17e6ff
Mehmet Alpkaya <alpkaya71@gmail.com>
2023-04-19 16:13:59 +0300

    Danger Zones and Obstacle

--- src/BattleLoc.java
+++ src/BattleLoc.java
@@ -1,9 +1,33 @@
-public class BattleLoc extends Location{
-    public BattleLoc(Player player, String name) {
+public abstract class BattleLoc extends Location{
+
+    private Obstacle obstacle; // BattleLoc have a obstacle
+    private String award;
+    public BattleLoc(Player player, String name, Obstacle obstacle, String award) {
         super(player, name);
+        this.obstacle=obstacle;
+        this.award=award;
     }
     @Override
     public boolean onLocation() {
-        return false;
+        System.out.println("you are in "+this.getName() );
+        System.out.println("Be careful");
+
+        return true;
+    }
+
+    public Obstacle getObstacle() {
+        return obstacle;
+    }
+
+    public void setObstacle(Obstacle obstacle) {
+        this.obstacle = obstacle;
+    }
+
+    public String getAward() {
+        return award;
+    }
+
+    public void setAward(String award) {
+        this.award = award;
     }
 }
