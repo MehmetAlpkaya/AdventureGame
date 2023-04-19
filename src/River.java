b9a8b0834bc62a431bcf999594e7b839deb641f4
Mehmet Alpkaya <alpkaya71@gmail.com>
2023-04-19 23:55:32 +0300

    Danger Zones and Obstacle

--- src/River.java
+++ src/River.java
@@ -1,5 +1,5 @@
 public class River extends BattleLoc{
     public River(Player player) {
-        super(player,"River",new Bear(),"Water");
+        super(player,"River",new Bear(),"Water",3);
     }
 }
